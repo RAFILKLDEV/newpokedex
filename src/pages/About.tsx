6cb35092788437d279d80aecf1e055c65eb6848f
Rafael Almeida Penha <rafaelalmeidapenha@gmail.com>
2023-02-23 16:43:43 -0300

    Finalizando mais um projeto dessa vez com Nextjs

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -9,6 +9,8 @@ export default function Sobre() {
       <GlobalStyleS />
       <Header></Header>
       <Content>
+        <h1>Obrigado por acompanhar mais um projeto da minha carreira de Front-End!</h1>
+        <h1>Github: <a href="https://github.com/RAFILKLDEV">https://github.com/RAFILKLDEV</a></h1>
       </Content>
     </>
   );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,24 +3,17 @@ import { Content } from "../components/Content";
 import { Header } from "../components/Header";
 import { GlobalStyleS } from "../styles/GlobalStyles";
 
-export async function getStaticProps() {
-  const data = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
-  const pokemon = await data.json();
-  console.log(pokemon.name);
-
-  return {
-    props: { pokemon },
-  };
-}
-
-export default function Home({ pokemon }) {
+export default function Home() {
   return (
     <>
       <GlobalStyleS />
       <Header></Header>
       <Content>
         <h1>Seja Bem vindo a mais um projeto Front-End do RAFILKLDEV!</h1>
-        <h2>Esse projeto tem em vista utilizar TypeScript, Next.js, Styled-Components, React e Jest!</h2>
+        <h2>
+          Esse projeto tem em vista utilizar TypeScript, Next.js,
+          Styled-Components e React!
+        </h2>
         <h2>A Pokedex esta sendo renderizada em Server render!</h2>
       </Content>
     </>
